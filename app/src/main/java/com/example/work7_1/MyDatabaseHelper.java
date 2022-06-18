32119044bda541b09d0a20abf6b4c82190993076
longyu5 <503436697@qq.com>
2022-06-18 23:04:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/work7_1/MyDatabaseHelper.java
@@ -0,0 +1,33 @@
+package com.example.work7_1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    public static final String CREATE_contacts = "create table contacts("
+            +"id integer primary key autoincrement,"
+            +"name text,"
+            +"phone text,"
+            +"sex text)";
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
+        super(context,name,factory,version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_contacts);
+//        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+
+
+}
